575d558c32de810988aa5efcdcce8e29a71ead52
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2023-06-05 22:04:59 -0400

    trying to get a new github auth working

--- myModule.js
+++ myModule.js
@@ -1,4 +1,5 @@
-import { Octokit } from "https://esm.sh/octokit@2.0.19";
+// import { Octokit } from "https://esm.sh/octokit@2.0.19";
+import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
 export function showPopup() {
     $('.ui.modal').modal('show');
@@ -7,23 +8,24 @@ export function showPopup() {
 export function grantAccessToRepo(repo) {
     var username = document.getElementById("username").value;
     const octokit = new Octokit({
-        auth: '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'
+        auth: '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'
     })
     
-    octokit.request('PUT /repos/{owner}/{repo}/collaborators/{username}', {
+    octokit.rest.repos.addCollaborator({
         owner: 'avahajr',
         repo: repo,
-        username: username,
-        permission: 'triage',
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-        }
-    })
+        name: username,
+    });
     closeModal();		
 }
+
+
+
+
 // Close the open modal.
 export function closeModal() {
     $('.ui.modal').modal('hide');
+    document.getElementById("username").reset;
 }
 
 window.showPopup=showPopup;

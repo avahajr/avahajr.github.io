853a167ce95d9332f1d2366d42f33cda061289ee
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-11-12 00:24:52 -0500

    min height for typer

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -33,16 +33,16 @@ export default function About() {
       <h1 className="text-6xl sm:text-7xl font-semibold mb-8">
         Hi, I&#39;m Ava.
       </h1>
-      <p className=" text-2xl sm:text-3xl">
+      <p className="text-2xl sm:text-3xl">
         I’m a recent Columbia grad with{" "}
         <span className="font-semibold">proven leadership experience</span>,{" "}
         <span className="font-semibold">excellent communication skills</span>,
         and a passion for developing new features using{" "}
         <span className="font-semibold">cutting-edge technologies</span>.
       </p>
-      <p className="text-xl mt-8">
+      <p className="text-xl mt-8 min-h-24">
         I&#39;m currently a software engineer at WBAR Radio, where I{" "}
-        <span ref={typedRef} className="font-medium min-h-80" id="typed" />
+        <span ref={typedRef} className="font-medium" id="typed" />
       </p>
       <Link
         isExternal
